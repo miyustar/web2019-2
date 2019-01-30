ec1e0fa4e94f793dff5fd7ced08c88d92ba018cd
zm17438 <zm17438@josai.ac.jp>
2019-01-29 22:13:56 -0500

    message

---
+++ m14.js
@@ -0,0 +1,4 @@
+function nazo1(){
+if (document.getElementById('answer1').value=='K-pop'){alert('あたり!');}
+else{alert('はずれ！');}
+}
---
+++ m15.js
@@ -0,0 +1,9 @@
+function seven66() {
+  who66 = 'seventeen'
+  alert('正解');
+}
+
+function wanna66() {
+   who66 = 'wannaone'
+  alert('正解');
+}
